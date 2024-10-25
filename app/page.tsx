82b1331081bc6d2c09f31e4195792e9afe9e1d91
juan arango <juanarangozapata35@gmail.com>
2024-10-25 17:55:42 -0500

    agrega page principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1 className="text-5xl">Hola mundo</h1>
+      <h1 className="text-5xl">Hola mundo</h1>
+      <Link href={"/about"}>About Page</Link>
     </div>
   );
 }
